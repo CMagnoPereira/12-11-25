260b554e4a53e32a7fd2b2ae398d89514fcbcfa0
CMagnoPereira <126297569+CMagnoPereira@users.noreply.github.com>
2024-11-14 01:36:14 +0000

    Initial commit

---
+++ js/principal.js
@@ -0,0 +1,12 @@
+var titulo = document.querySelector("h1");
+titulo.textContent = "Carlos Magno Nutrição";
+
+var subtitulo = document.querySelector("h2");
+subtitulo.textContent = "Pacientes cadastrado"
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var nomeTd = paciente.querySelector(".info-nome");
+var pesoTd = paciente.querySelector("info-peso");
+var alturaTd = paciente.querySelector("info-altura");
+var goruraTd = paciente.querySelector("info-gordura");
